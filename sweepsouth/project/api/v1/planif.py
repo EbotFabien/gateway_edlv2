665e9f78239b8fe68ad3dd4d0139cc675ee9291d
fabienclassic <fabienclassic@gmail.com>
2023-05-10 19:18:05 +0100

    done

--- sweepsouth/project/api/v1/planif.py
+++ sweepsouth/project/api/v1/planif.py
@@ -20,15 +20,15 @@ def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         token = None
-        if 'API-KEY' in request.headers:
-            token = request.headers['API-KEY']
-            try:
-                data = jwt.decode(token, app.config.get('SECRET_KEY'))
-            except:
-                return {'message': 'Token is invalid.'}, 403
+        if 'Authorization' in request.headers:
+            token = request.headers['Authorization']
+            '''URL="http://195.15.218.172/security/manager_app/viewset/role/"
+            params ={"token":token}
+            r = requests.post(url=URL,params=params)
+            print(r)'''
         if not token:
             return {'message': 'Token is missing or not found.'}, 401
-        if data:
+        if token :
             pass
         return f(*args, **kwargs)
     return decorated
@@ -44,6 +44,11 @@ planif  = planif1.namespace('/api/planif', \
     description= "All routes under this section of the documentation are the open routes bots can perform CRUD action \
     on the application.", \
     path = '/v1/')
+
+
+parti= planif.model('participant', {
+    "nom": fields.String(required=False,default=" ", description="Users nom"),
+})
 
 
 @planif.doc(
@@ -143,6 +148,7 @@ class usera(Resource):
                     "res":"User service down"
                 }, 400
 
+#edl routes
 @planif.doc(
     security='KEY',
     params={'start': 'Value to start from ',
@@ -162,8 +168,8 @@ class usera(Resource):
         404: 'Resource Not found',
         500: 'internal server error, please contact admin and report issue'
     })
-@planif.route('planif/edl/all')
-class edla(Resource):
+@planif.route('/planif/edl/all')
+class edlall(Resource):
     def get(self):
         if request.args:
             start = request.args.get('start', None)
@@ -175,7 +181,7 @@ class edla(Resource):
             previous = "/api/v1/post/tags?start=" + \
                 str(int(start)-1)+"&limit="+limit+"&count="+count
             
-            URL="http://195.15.218.172/edluser/Admin/tous"
+            URL="http://195.15.218.172/edluser/edl/tous"
             r = requests.get(url=URL)
             if r.status_code == 200:
                 return {
@@ -193,6 +199,143 @@ class edla(Resource):
 
 @planif.doc(
     security='KEY',
+    params={},
+
+    responses={
+        200: 'ok',
+        201: 'created',
+        204: 'No Content',
+        301: 'Resource was moved',
+        304: 'Resource was not Modified',
+        400: 'Bad Request to server',
+        401: 'Unauthorized request from client to server',
+        403: 'Forbidden request from client to server',
+        404: 'Resource Not found',
+        500: 'internal server error, please contact admin and report issue'
+    })
+@planif.route('/planif/edl/add')
+class edl_add(Resource):
+    @token_required
+    @planif.expect(parti)
+    def post(self):
+        req_data = request.get_json()
+        token=request.headers['Authorization']
+        if token:
+            URL="http://195.15.218.172/participant/edl/ajouter"
+            r = requests.post(url=URL,json=req_data)
+            if r.status_code == 200 :
+                return {
+                        'status': 1,
+                        'res': r.json(),
+                    }, 200
+            else:
+                return {
+                        'status':0,
+                        'res': 'failed',
+                    }, 400
+        else:
+                return {
+                        'status':0,
+                        'res': 'input token',
+                    }, 403
+
+@planif.doc(
+    security='KEY',
+    params={},
+
+    responses={
+        200: 'ok',
+        201: 'created',
+        204: 'No Content',
+        301: 'Resource was moved',
+        304: 'Resource was not Modified',
+        400: 'Bad Request to server',
+        401: 'Unauthorized request from client to server',
+        403: 'Forbidden request from client to server',
+        404: 'Resource Not found',
+        500: 'internal server error, please contact admin and report issue'
+    })
+@planif.route('/planif/edl/update')
+class edl_update(Resource):
+    @token_required
+    @planif.expect(parti)
+    def put(self):
+        req_data = request.json
+        
+        token=request.headers['Authorization']
+        if token:
+            URL="http://195.15.218.172/participant/edl/modifier/"+req_data['id']
+            del req_data['id']
+            r = requests.post(url=URL,json=req_data)
+            if r.status_code == 200 :
+                return {
+                        'status': 1,
+                        'res': r.json(),
+                    }, 200
+            else:
+                return {
+                        'status':0,
+                        'res': 'failed',
+                    }, 400
+        else:
+                return {
+                        'status':0,
+                        'res': 'input token',
+                    }, 403
+
+@planif.doc(
+    security='KEY',
+    params={'start': 'Value to start from ',
+             'limit': 'Total limit of the query',
+             'count': 'Number results per page',
+            'id': 'id'
+            },
+    responses={
+        200: 'ok',
+        201: 'created',
+        204: 'No Content',
+        301: 'Resource was moved',
+        304: 'Resource was not Modified',
+        400: 'Bad Request to server',
+        401: 'Unauthorized request from client to server',
+        403: 'Forbidden request from client to server',
+        404: 'Resource Not found',
+        500: 'internal server error, please contact admin and report issue'
+    })
+@planif.route('/planif/edl/indivi')
+class edlsing(Resource):
+    def get(self):
+        if request.args:
+            start = request.args.get('start', None)
+            limit = request.args.get('limit', None)
+            count = request.args.get('count', None)
+            ID = request.args.get('id', None)
+            # Still to fix the next and previous WRT Sqlalchemy
+            next = "/api/v1/post/tags?start=" + \
+                str(int(start)+1)+"&limit="+limit+"&count="+count
+            previous = "/api/v1/post/tags?start=" + \
+                str(int(start)-1)+"&limit="+limit+"&count="+count
+            
+            URL="http://195.15.218.172/edluser/edl/"+ID
+            r = requests.get(url=URL)
+            if r.status_code == 200:
+                return {
+                    "start": start,
+                    "limit": limit,
+                    "count": count,
+                    "next": next,
+                    "previous": previous,
+                    "results":r.json()
+                }, 200
+            else:
+                return{
+                    "res":"Planif edl service down"
+                }, 400
+
+
+#users  route
+@planif.doc(
+    security='KEY',
     params={'start': 'Value to start from ',
              'limit': 'Total limit of the query',
              'count': 'Number results per page',
@@ -210,8 +353,8 @@ class edla(Resource):
         404: 'Resource Not found',
         500: 'internal server error, please contact admin and report issue'
     })
-@planif.route('planif/rdv/all')
-class rdva(Resource):
+@planif.route('/planif/users/all')
+class usera(Resource):
     def get(self):
         if request.args:
             start = request.args.get('start', None)
@@ -223,7 +366,194 @@ class rdva(Resource):
             previous = "/api/v1/post/tags?start=" + \
                 str(int(start)-1)+"&limit="+limit+"&count="+count
             
-            URL="http://195.15.218.172/edluser/Admin/tous"
+            URL="http://195.15.218.172/edluser/user/tous"
+            r = requests.get(url=URL)
+            if r.status_code == 200:
+                return {
+                    "start": start,
+                    "limit": limit,
+                    "count": count,
+                    "next": next,
+                    "previous": previous,
+                    "results":r.json()
+                }, 200
+            else:
+                return{
+                    "res":"User service down"
+                }, 400
+
+@planif.doc(
+    security='KEY',
+    responses={
+        200: 'ok',
+        201: 'created',
+        204: 'No Content',
+        301: 'Resource was moved',
+        304: 'Resource was not Modified',
+        400: 'Bad Request to server',
+        401: 'Unauthorized request from client to server',
+        403: 'Forbidden request from client to server',
+        404: 'Resource Not found',
+        500: 'internal server error, please contact admin and report issue'
+    })
+@planif.route('/planif/update/users')
+class user_Update(Resource):
+    @token_required
+    @planif.expect(parti)
+    def put(self):
+        user_data = request.get_json()
+        token = request.headers['Authorization']
+        if token:
+            
+            URL="http://195.15.218.172/agent_app/user/update/"+str(user_data['id'])
+            headers ={"Authorization":token}
+            #for key,value in user_data.iteritems():
+            #    if value == None:
+            #        del user_data[key]
+
+            del user_data['id']
+            print(user_data)
+            r = requests.post(url=URL,headers=headers,json=user_data)
+            print(r.headers)
+               
+                
+        else:
+                return {
+                        'status':0,
+                        'res': 'no data',
+                    }, 401
+
+
+#participant route
+
+@planif.doc(
+    security='KEY',
+    params={'start': 'Value to start from ',
+             'limit': 'Total limit of the query',
+             'count': 'Number results per page',
+            
+            },
+    responses={
+        200: 'ok',
+        201: 'created',
+        204: 'No Content',
+        301: 'Resource was moved',
+        304: 'Resource was not Modified',
+        400: 'Bad Request to server',
+        401: 'Unauthorized request from client to server',
+        403: 'Forbidden request from client to server',
+        404: 'Resource Not found',
+        500: 'internal server error, please contact admin and report issue'
+    })
+@planif.route('/planif/participant/all')
+class planifparticipanta(Resource):
+    @token_required
+    def get(self):
+        if request.args:
+            start = request.args.get('start', None)
+            limit = request.args.get('limit', None)
+            count = request.args.get('count', None)
+            # Still to fix the next and previous WRT Sqlalchemy
+            next = "/api/v1/post/tags?start=" + \
+                str(int(start)+1)+"&limit="+limit+"&count="+count
+            previous = "/api/v1/post/tags?start=" + \
+                str(int(start)-1)+"&limit="+limit+"&count="+count
+            
+            URL="http://195.15.218.172/participant/participant/tous"
+            r = requests.get(url=URL)
+            if r.status_code == 200:
+                return {
+                    "start": start,
+                    "limit": limit,
+                    "count": count,
+                    "next": next,
+                    "previous": previous,
+                    "results": r.json()
+                }, 200
+            else:
+                return{
+                    "res":"planif service down"
+                }, 400
+
+
+
+@planif.doc(
+    security='KEY',
+    params={},
+
+    responses={
+        200: 'ok',
+        201: 'created',
+        204: 'No Content',
+        301: 'Resource was moved',
+        304: 'Resource was not Modified',
+        400: 'Bad Request to server',
+        401: 'Unauthorized request from client to server',
+        403: 'Forbidden request from client to server',
+        404: 'Resource Not found',
+        500: 'internal server error, please contact admin and report issue'
+    })
+@planif.route('/planif/participant/add')
+class planif_Parti_add(Resource):
+    @token_required
+    @planif.expect(parti)
+    def post(self):
+        req_data = request.get_json()
+        token=request.headers['Authorization']
+        if token:
+            URL="http://195.15.218.172/participant/participant/ajouter"
+            r = requests.post(url=URL,json=req_data)
+            if r.status_code == 200 :
+                return {
+                        'status': 1,
+                        'res': r.json(),
+                    }, 200
+            else:
+                return {
+                        'status':0,
+                        'res': 'failed',
+                    }, 400
+        else:
+                return {
+                        'status':0,
+                        'res': 'input token',
+                    }, 403
+
+
+#rdv route
+@planif.doc(
+    security='KEY',
+    params={'start': 'Value to start from ',
+             'limit': 'Total limit of the query',
+             'count': 'Number results per page',
+            'category': 'category'
+            },
+    responses={
+        200: 'ok',
+        201: 'created',
+        204: 'No Content',
+        301: 'Resource was moved',
+        304: 'Resource was not Modified',
+        400: 'Bad Request to server',
+        401: 'Unauthorized request from client to server',
+        403: 'Forbidden request from client to server',
+        404: 'Resource Not found',
+        500: 'internal server error, please contact admin and report issue'
+    })
+@planif.route('/planif/rdv/all')
+class rdvall(Resource):
+    def get(self):
+        if request.args:
+            start = request.args.get('start', None)
+            limit = request.args.get('limit', None)
+            count = request.args.get('count', None)
+            # Still to fix the next and previous WRT Sqlalchemy
+            next = "/api/v1/post/tags?start=" + \
+                str(int(start)+1)+"&limit="+limit+"&count="+count
+            previous = "/api/v1/post/tags?start=" + \
+                str(int(start)-1)+"&limit="+limit+"&count="+count
+            
+            URL="http://195.15.218.172/edluser/rdv/tous"
             r = requests.get(url=URL)
             if r.status_code == 200:
                 return {
@@ -238,3 +568,139 @@ class rdva(Resource):
                 return{
                     "res":"Planif edl service down"
                 }, 400
+
+@planif.doc(
+    security='KEY',
+    params={},
+
+    responses={
+        200: 'ok',
+        201: 'created',
+        204: 'No Content',
+        301: 'Resource was moved',
+        304: 'Resource was not Modified',
+        400: 'Bad Request to server',
+        401: 'Unauthorized request from client to server',
+        403: 'Forbidden request from client to server',
+        404: 'Resource Not found',
+        500: 'internal server error, please contact admin and report issue'
+    })
+@planif.route('/planif/rdv/add')
+class rdv_add(Resource):
+    @token_required
+    @planif.expect(parti)
+    def post(self):
+        req_data = request.get_json()
+        token=request.headers['Authorization']
+        if token:
+            URL="http://195.15.218.172/participant/rdv/ajouter"
+            r = requests.post(url=URL,json=req_data)
+            if r.status_code == 200 :
+                return {
+                        'status': 1,
+                        'res': r.json(),
+                    }, 200
+            else:
+                return {
+                        'status':0,
+                        'res': 'failed',
+                    }, 400
+        else:
+                return {
+                        'status':0,
+                        'res': 'input token',
+                    }, 403
+
+@planif.doc(
+    security='KEY',
+    params={},
+
+    responses={
+        200: 'ok',
+        201: 'created',
+        204: 'No Content',
+        301: 'Resource was moved',
+        304: 'Resource was not Modified',
+        400: 'Bad Request to server',
+        401: 'Unauthorized request from client to server',
+        403: 'Forbidden request from client to server',
+        404: 'Resource Not found',
+        500: 'internal server error, please contact admin and report issue'
+    })
+@planif.route('/planif/rdv/update')
+class rdv_update(Resource):
+    @token_required
+    @planif.expect(parti)
+    def put(self):
+        req_data = request.json
+        
+        token=request.headers['Authorization']
+        if token:
+            URL="http://195.15.218.172/participant/rdv/update/"+req_data['id']
+            del req_data['id']
+            r = requests.post(url=URL,json=req_data)
+            if r.status_code == 200 :
+                return {
+                        'status': 1,
+                        'res': r.json(),
+                    }, 200
+            else:
+                return {
+                        'status':0,
+                        'res': 'failed',
+                    }, 400
+        else:
+                return {
+                        'status':0,
+                        'res': 'input token',
+                    }, 403
+
+@planif.doc(
+    security='KEY',
+    params={'start': 'Value to start from ',
+             'limit': 'Total limit of the query',
+             'count': 'Number results per page',
+            'id': 'id'
+            },
+    responses={
+        200: 'ok',
+        201: 'created',
+        204: 'No Content',
+        301: 'Resource was moved',
+        304: 'Resource was not Modified',
+        400: 'Bad Request to server',
+        401: 'Unauthorized request from client to server',
+        403: 'Forbidden request from client to server',
+        404: 'Resource Not found',
+        500: 'internal server error, please contact admin and report issue'
+    })
+@planif.route('/planif/rdv/indivi')
+class rdvsing(Resource):
+    def get(self):
+        if request.args:
+            start = request.args.get('start', None)
+            limit = request.args.get('limit', None)
+            count = request.args.get('count', None)
+            ID = request.args.get('id', None)
+            # Still to fix the next and previous WRT Sqlalchemy
+            next = "/api/v1/post/tags?start=" + \
+                str(int(start)+1)+"&limit="+limit+"&count="+count
+            previous = "/api/v1/post/tags?start=" + \
+                str(int(start)-1)+"&limit="+limit+"&count="+count
+            
+            URL="http://195.15.218.172/edluser/rdv/"+ID
+            r = requests.get(url=URL)
+            if r.status_code == 200:
+                return {
+                    "start": start,
+                    "limit": limit,
+                    "count": count,
+                    "next": next,
+                    "previous": previous,
+                    "results":r.json()
+                }, 200
+            else:
+                return{
+                    "res":"Planif edl service down"
+                }, 400
+
