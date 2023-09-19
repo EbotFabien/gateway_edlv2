559932a35d3f8fabf526f721eda3a49f49ce26ad
Ebot <touchone0001@gmail.com>
2023-09-19 20:01:18 +0200

    'one'

--- sweepsouth/project/api/v1/logement.py
+++ sweepsouth/project/api/v1/logement.py
@@ -390,8 +390,39 @@ class compteursmod(Resource):
                         'res': 'input token',
                     }, 403
 
+#by_ref
+@logement.doc(
+    security='KEY',
+    params={'ref': 'ref logement'
+            },
+    responses={
+        200: 'ok',
+        201: 'created',
+        204: 'No Content',
+        301: 'Resource was moved',
+        304: 'Resource was not Modified',
+        400: 'Bad Request to server',
+        401: 'Unauthorized request from client to server',
+        403: 'Forbidden request from client to server',
+        404: 'Resource Not found',
+        500: 'internal server error, please contact admin and report issue'
+    })
+@logement.route('/logement/logement/by_ref/')
+class logement_byref(Resource):
+    def get(self):
+        if request.args:
+            start = request.args.get('ref', None)
+            URL="http://195.15.228.250/logement/logement/by_ref/"+start
+            r = requests.get(url=URL)
+            if r.status_code == 200:
+                return {
 
-
+                    "results":r.json()
+                }, 200
+            else:
+                return{
+                    "res":"logement by ref service down"
+                }, 400
 #client route
 @logement.doc(
     security='KEY',
